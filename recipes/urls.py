d0568188b488a0cd19c2a165fa4f6ecb8ea82641
Matheus Henrique <matheushnt06@gmail.com>
2025-01-12 16:39:36 -0300

    Removido sobre e contato

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('sobre/', views.sobre),
-    path('contato/', views.contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contatos')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Matheus Henrique'
+    })
