f8a469fe893e5bdac5ab43e567b9f41917fe5d99
Matheus Henrique <matheushnt06@gmail.com>
2025-01-13 21:40:07 -0300

    Uma nova URL para uma nova View - sobre URLs no Django

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Matheus Henrique'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Matheus Henrique'
+    })
