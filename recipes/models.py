5900a6f6983cae98afb71f76e5d1d23dd78b68a4
Matheus Henrique <matheushnt06@gmail.com>
2025-01-15 20:39:52 -0300

    Corrigindo category com blank True e default None

--- recipes/models.py
+++ recipes/models.py
@@ -29,7 +29,9 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
+        blank=True,
+        default=None
     )
     author = models.ForeignKey(
         User,
