513a62b9c67c73096457e83b37ac260073afb781
Matheus Henrique <matheushnt06@gmail.com>
2025-01-23 18:51:59 -0300

    Criando testes parametrizados PT. 1

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -12,3 +12,17 @@ class RecipeModelTest(RecipeTestBase):
 
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_recipe_fields_max_length(self):
+        fields = [
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65),
+        ]
+
+        for field, max_length in fields:
+            with self.subTest(field=field, max_length=max_length):
+                setattr(self.recipe, field, 'A' * (max_length + 0))
+                with self.assertRaises(ValidationError):
+                    self.recipe.full_clean()
