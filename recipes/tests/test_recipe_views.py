0afd72562bcfd3ed647c2f277357bf55725102a3
Matheus Henrique <matheushnt06@gmail.com>
2025-01-26 14:08:22 -0300

    Seus teste vão prevenir regressões no seu código (temos erros em outros testes)

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -125,7 +125,7 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(view.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:search'))
+        response = self.client.get(reverse('recipes:search') + '?q=testing')
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
 
     def test_recipe_search_raises_404_if_no_search_term(self):
