149a5853cfa6869488962cf2e694787ab05a33b5
Matheus Henrique <matheushnt06@gmail.com>
2025-01-27 21:33:31 -0300

    Testando request.GET - Usando URL Query String com o client de testes do Django

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -22,3 +22,33 @@ class RecipeSearchViewTest(RecipeTestBase):
         response = self.client.get(url)
         content = response.content.decode('utf-8')
         self.assertIn('Search for &quot;Receita de bolo&quot; | ', content)
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'This is recipe one'
+        title2 = 'This is recipe two'
+
+        self.make_recipe(
+            author={'username': 'one'},
+            slug='one',
+            title=title1,
+        )
+        self.make_recipe(
+            author={'username': 'two'},
+            slug='two',
+            title=title2,
+        )
+
+        url = reverse('recipes:search')
+        response1 = self.client.get(f'{url}?q={title1}')
+        response2 = self.client.get(f'{url}?q={title2}')
+        response_both = self.client.get(url + '?q=this')
+
+        self.assertIn(title1, response1.content.decode('utf-8'))
+        self.assertNotIn(title2, response1.content.decode('utf-8'))
+
+        self.assertIn(title2, response2.content.decode('utf-8'))
+        self.assertNotIn(title1, response2.content.decode('utf-8'))
+
+        self.assertIn('this', response_both.content.decode('utf-8'))
+
+        # It is possible to make assertions through context of the response
