05d53bc041d17a3c6e08cd960818bf7511139b6f
Matheus Henrique <matheushnt06@gmail.com>
2025-02-04 18:08:15 -0300

    Configurando o formulário com labels, widgets, erros, atributos dos campos, etc

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,33 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        # exclude = ['first_name']
+
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'Email',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'The e-mail must be valid.'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty.',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here',
+                'class': 'input text-input',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            }),
+        }
