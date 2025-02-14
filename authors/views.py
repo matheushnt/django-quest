6978234930a6a21aef1f8481b9fd94e57a6acb4b
Matheus Henrique <matheushnt06@gmail.com>
2025-02-14 14:34:21 -0300

    Corrigindo a criptografia de senha do usuário registrado pela nossa aplicação

--- authors/views.py
+++ authors/views.py
@@ -27,7 +27,9 @@ def create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user has been created, please log in')
 
         del request.session['register_form_data']
