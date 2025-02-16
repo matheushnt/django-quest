b0d3aa551b59093886e2b50485660b3050fd66c4
Matheus Henrique <matheushnt06@gmail.com>
2025-02-16 20:54:20 -0300

    Autenticando o usuário com authenticate e login

--- authors/forms/login.py
+++ authors/forms/login.py
@@ -3,8 +3,8 @@ from utils.django_forms import add_placeholder
 
 
 class LoginForm(forms.Form):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         add_placeholder(self.fields['username'], 'Type your username')
         add_placeholder(self.fields['password'], 'Type your password')
 
