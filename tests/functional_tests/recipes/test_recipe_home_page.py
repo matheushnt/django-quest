07630781788b0ff44850801ff9bf51508d8ae9f6
Matheus Henrique <matheushnt06@gmail.com>
2025-02-21 14:38:58 -0300

    Agrupando testes funcionais com pytest markers

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,7 +1,9 @@
 from .base import RecipeBaseFunctionalTest
 from selenium.webdriver.common.by import By
+import pytest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
