a0820faba6a28ffbbd8ffe22b5c0fd92ba7972a9
Matheus Henrique <matheushnt06@gmail.com>
2025-02-24 12:51:42 -0300

    Adicionando marks no testes funcionais

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from .base import AuthorsBaseTest
+import pytest
 
 
+@pytest.mark.functional_test
 class AuthorsRegisterTest(AuthorsBaseTest):
     def fill_form_dummy_data(self, form):
         fields = form.find_elements(
