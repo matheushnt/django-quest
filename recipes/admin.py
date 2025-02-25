44de216dacaf22a32c3fa35607f776fbce19e14c
Matheus Henrique <matheushnt06@gmail.com>
2025-02-25 11:09:11 -0300

    Ajustando a área administrativa padrão do Django (Admin)

--- recipes/admin.py
+++ recipes/admin.py
@@ -17,4 +17,15 @@ admin.site.register(Category, CategoryAdmin)
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'title', 'created_at', 'is_published', 'author',)
+    list_display_links = ('title', 'created_at',)
+    search_fields = ('id', 'title', 'description', 'slug', 'preparation_step',)
+    list_filter = (
+        'category',
+        'author',
+        'is_published',
+        'preparation_steps_is_html',
+    )
+    list_per_page = 10
+    list_editable = ('is_published',)
+    ordering = ('-id',)
