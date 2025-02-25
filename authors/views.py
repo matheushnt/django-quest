55549c2cb4464e70b45562f4d5b1e429e4a5f461
Matheus Henrique <matheushnt06@gmail.com>
2025-02-25 14:00:33 -0300

    Criando um template, url e view para o usuário editar a receita

--- authors/views.py
+++ authors/views.py
@@ -105,3 +105,17 @@ def dashboard(request):
     }
 
     return render(request, 'authors/pages/dashboard.html', context)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_edit(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=False,
+        author=request.user,
+    )
+
+    if not recipe:
+        raise Http404()
+
+    return render(request, 'authors/pages/dashboard_recipe.html')
