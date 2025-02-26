5a1337d287f69cb62bae67b3a9ee3133ffaf67d3
Matheus Henrique <matheushnt06@gmail.com>
2025-02-26 20:30:16 -0300

    Criando uma view para criação de receitas

--- authors/views.py
+++ authors/views.py
@@ -141,3 +141,33 @@ def dashboard_recipe_edit(request, id):
     }
 
     return render(request, 'authors/pages/dashboard_recipe.html', context)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(
+        request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Your recipe has been saved successfully')
+
+        return redirect(reverse(
+            'authors:dashboard_recipe_edit',
+            args=(recipe.id,)
+        ))
+
+    context = {
+        'form': form,
+        'form_action': reverse('authors:dashboard_recipe_new'),
+    }
+
+    return render(request, 'authors/pages/dashboard_recipe.html', context)
