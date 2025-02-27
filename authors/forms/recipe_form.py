2bd9bda75e600809b0c6c04636c6f6a22cf9b3d4
Matheus Henrique <matheushnt06@gmail.com>
2025-02-26 21:45:49 -0300

    Validando o formulário de cadastrar/editar receitas

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,11 +1,13 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from recipes.models import Recipe
-from utils.django_forms import add_attr
+from utils.django_forms import add_attr, is_positive_number
 
 
 class AuthorRecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.__errors_form = {}
 
         add_attr(self.fields.get('preparation_step'), 'class', 'span-2')
 
@@ -42,3 +44,47 @@ class AuthorRecipeForm(forms.ModelForm):
                 )
             ),
         }
+
+    def clean(self):
+        super().clean()
+        cleaned_data = self.cleaned_data
+
+        title = cleaned_data.get('title')
+        description = cleaned_data.get('description')
+
+        if title and len(title) < 5:
+            self.__errors_form.setdefault('title', []).append(
+                'Must have at least 5 chars')
+
+        if title == description:
+            self.__errors_form.setdefault('title', []).append(
+                'Cannot be equal to description'
+            )
+            self.__errors_form.setdefault('description', []).append(
+                'Cannot be equal to title'
+            )
+
+        if self.__errors_form:
+            raise ValidationError(self.__errors_form)
+
+        return cleaned_data
+
+    def clean_preparation_time(self):
+        field_value = self.cleaned_data.get('preparation_time')
+
+        if not is_positive_number(field_value):
+            self.__errors_form.setdefault('preparation_time', []).append(
+                'Must be a positive number'
+            )
+
+        return field_value
+
+    def clean_servings(self):
+        field_value = self.cleaned_data.get('servings')
+
+        if not is_positive_number(field_value):
+            self.__errors_form.setdefault('servings', []).append(
+                'Must be a positive number'
+            )
+
+        return field_value
--- utils/django_forms.py
+++ utils/django_forms.py
@@ -22,3 +22,12 @@ def strong_password(password):
         ),
             code='invalid'
         )
+
+
+def is_positive_number(value):
+    try:
+        number_string = float(value)
+    except ValueError:
+        return False
+
+    return number_string > 0
