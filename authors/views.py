6a010f89716b689fb4b3126242d470df614d4a2b
Matheus Henrique <matheushnt06@gmail.com>
2025-02-26 22:59:22 -0300

    CRUD com delete para apagar receitas de forma segura + JavaScript adicional

--- authors/views.py
+++ authors/views.py
@@ -171,3 +171,19 @@ def dashboard_recipe_new(request):
     }
 
     return render(request, 'authors/pages/dashboard_recipe.html', context)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_delete(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=False,
+        author=request.user,
+    ).first()
+
+    if not recipe:
+        raise Http404()
+
+    recipe.delete()
+    messages.success(request, 'Deleted successfully')
+    return redirect('authors:dashboard')
