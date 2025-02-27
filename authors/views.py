1cb622c4b6e6168a7c79a5e4afcde4eb26ca17c0
Matheus Henrique <matheushnt06@gmail.com>
2025-02-26 23:09:02 -0300

    Permitindo apenas POST na view para apagar receitas

--- authors/views.py
+++ authors/views.py
@@ -174,7 +174,12 @@ def dashboard_recipe_new(request):
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_delete(request, id):
+def dashboard_recipe_delete(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    id = POST.get('id')
     recipe = Recipe.objects.filter(
         pk=id,
         is_published=False,
